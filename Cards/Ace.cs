ac9b5cedc825dc0c306d2b2e67de6bd79e3ab58d
Liam-Dombrowski-Student <230006210+Liam-Dombrowski-Student@users.noreply.github.com>
2025-10-28 14:40:29 -0400

    Working on DrawCardFromDeck override method

--- Cards/Ace.cs
+++ Cards/Ace.cs
@@ -3,5 +3,8 @@ namespace Cards
     public class Ace : Card
     {
         protected int cardValue;
+        protected string cardName;
+
+        public 
     }
 }
--- Cards/Card.cs
+++ Cards/Card.cs
@@ -1,5 +1,10 @@
 namespace Cards
 {
     public class Card
-    { ;}
+    {
+        public virtual void DrawCardFromDeck()
+        {
+            Console.WriteLine("You drew a card!");
+        }
+    }
 }
