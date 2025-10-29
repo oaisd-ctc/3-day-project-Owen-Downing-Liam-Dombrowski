81e89c7cc6daa8d3223ead148b8225785ff22fbd
Owen-Downing-Student <owdo340@student.oaisd.org>
2025-10-29 13:01:11 -0400

    Added PlayGame function to draw both dealer and player hands

--- Program.cs
+++ Program.cs
@@ -15,5 +15,19 @@ public class Program
 		return deckList[cardIndex];
 		deckList.Remove(deckList[cardIndex]);
 	}
-}
+
+	public void PlayGame()
+	{
+		object dealerCard = DrawCard();
+		object dealerHiddenCard = DrawCard();
+		Console.WriteLine($"The dealer drew a {dealerCard.GetName()} and a hidden card.");
+
+		object playerCard1 = DrawCard();
+		object playerCard2 = DrawCard();
+		Console.WriteLine($"You drew a {playerCard1.GetName()} and a {playerCard2.GetName()}.")
+		int handValue = playerCard1.GetValue() + playerCard2.GetValue();
+
+		PlayerTurn();
+
+	}
 }
